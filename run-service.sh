b9e292df4a60bc9e2fa04afc6939217f9665287f
Jastin nesto <jastinlasway10@gmail.com>
2024-09-10 15:41:20 +0300

    removing hyphen

--- run-service.sh
+++ run-service.sh
@@ -4,6 +4,6 @@ docker network create proxy
 
 touch acme.json && chmod 600 acme.json
 
-docker-compose up --build -d
+docker compose up  -build -d
 
-docker-compose exec sustainability python manage.py migrate --noinput
+docker compose exec sustainability python manage.py migrate --noinput
